2082115bafa70dbe0df269dcf5618cef083f3872
Сергей <@DESKTOP-FBSMLQR>
2026-03-12 03:43:22 +0700

    Добавьте файлы проекта.

---
+++ Core/TokenType.cs
@@ -0,0 +1,21 @@
+﻿namespace Laba1.Core
+{
+    public enum TokenType
+    {
+        UnsignedInteger,   // 1
+        Identifier,        // 2
+        KeywordNew,        // 3
+        KeywordString,     // 4
+        KeywordInt,        // 5
+        StringLiteral,     // 6
+        LessThan,          // 7
+        GreaterThan,       // 8
+        Comma,             // 9
+        Assign,            // 10
+        Whitespace,        // 11
+        OpenBrace,         // 12
+        CloseBrace,        // 13
+        Semicolon,         // 14
+        Error              // 99
+    }
+}
